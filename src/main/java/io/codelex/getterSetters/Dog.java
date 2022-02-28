b27b0befc90706730ea20d7be05f5aba6b901fd9
abulavs <andris.bulavs@gmail.com>
2022-03-01 00:39:28 +0200

    delete wrong folder + Getters/Setters

---
+++ src/main/java/io/codelex/getterSetters/Dog.java
@@ -0,0 +1,31 @@
+package io.codelex.getterSetters;
+
+public class Dog {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    private String name = "Dog";
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", years=" + years +
+                '}';
+    }
+
+    private int years = 5;
+}
---
+++ src/main/java/io/codelex/getterSetters/MainMetod.java
@@ -0,0 +1,10 @@
+package io.codelex.getterSetters;
+
+public class MainMetod {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.setName("Valis");
+        myDog.setYears(13);
+        System.out.println(myDog);
+    }
+}
