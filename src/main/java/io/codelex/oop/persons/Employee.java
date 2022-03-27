379185fe77a3e380264a7f14e37fea699a7af7a2
abulavs <andris.bulavs@gmail.com>
2022-03-27 17:16:08 +0300

    Update

--- src/main/java/io/codelex/oop/persons/Employee.java
+++ src/main/java/io/codelex/oop/persons/Employee.java
@@ -6,10 +6,10 @@ public class Employee extends Person {
     private String position;
     private LocalDate startedWorking;
 
-    public Employee(String firstName, String lastName, String id, int age, String position, LocalDate startedWorking) {
+    public Employee(String firstName, String lastName, String id, int age, String position, String startedWorking) {
         super(firstName, lastName, id, age);
         this.position = position;
-        this.startedWorking = startedWorking;
+        this.startedWorking = LocalDate.parse(startedWorking);
     }
 
     public String getPosition() {
